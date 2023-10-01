4cf2459a6ea195c20ff699a5e3d6dae790e9af9f
4samad <smd2k15@gmail.com>
2023-10-01 13:07:26 +0400

    femida cause image update

--- src/templates/exploreCauses.tsx
+++ src/templates/exploreCauses.tsx
@@ -9,33 +9,9 @@ const ExploreCauses = () => (
         <BoxFeature
           id="1"
           title="Help us Build Femida"
-          image="/assets/images/katt-yukawa-K0E6E0a0R3A-unsplash(1).jpg"
+          image="/assets/images/femida-cause-cover-image.png"
           description="Femida can be a massive contribution to solving world poverty or climate crisis by compelling people to invest more in these causes. Help us make this a reality."
           amount="$1000"
-          link="https://donate.stripe.com/cN215t5HG9Lrc809AK"
-        />
-        <BoxFeature
-          id="2"
-          title="Help us dasdasasd Femida"
-          image="/assets/images/katt-yukawa-K0E6E0a0R3A-unsplash(1).jpg"
-          description="Lorem ipsum doler sit amet"
-          amount="$400"
-          link="https://donate.stripe.com/cN215t5HG9Lrc809AK"
-        />
-        <BoxFeature
-          id="2"
-          title="Help us dasdasasd Femida"
-          image="/assets/images/katt-yukawa-K0E6E0a0R3A-unsplash(1).jpg"
-          description="Lorem ipsum doler sit amet"
-          amount="$400"
-          link="https://donate.stripe.com/cN215t5HG9Lrc809AK"
-        />
-        <BoxFeature
-          id="2"
-          title="Help us dasdasasd Femida"
-          image="/assets/images/katt-yukawa-K0E6E0a0R3A-unsplash(1).jpg"
-          description="Lorem ipsum doler sit amet"
-          amount="$400"
           link="https://donate.stripe.com/cN215t5HG9Lrc809AK"
         />
       </div>
